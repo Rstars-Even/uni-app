fa3d0ef791ef92b11258a262d0f754b1cfb94395
Rstars-Even <473753137@qq.com>
2024-01-31 18:09:11 +0800

    添加首页模块。

--- src/components/XtxSwiper.vue
+++ src/components/XtxSwiper.vue
@@ -2,46 +2,30 @@
 import { ref } from 'vue'
 const activeIndex = ref(0)
 const onChange = (ev) => {
-  activeIndex.value = ev.detail.current
+  activeIndex.value = ev.detail!.current //非空断言
 }
+
+// 接收父组件轮播图数据。
+const props = defineProps<{
+  list: BannerItem[]
+}>()
+console.log(props)
 </script>
 
 <template>
   <view class="carousel">
     <swiper :circular="true" :autoplay="false" :interval="3000" @change="onChange">
-      <swiper-item>
+      <swiper-item v-for="item in list" :key="item.id">
         <navigator url="/pages/index/index" hover-class="none" class="navigator">
-          <image
-            mode="aspectFill"
-            class="image"
-            src="https://pcapi-xiaotuxian-front-devtest.itheima.net/miniapp/uploads/slider_1.jpg"
-          ></image>
-        </navigator>
-      </swiper-item>
-      <swiper-item>
-        <navigator url="/pages/index/index" hover-class="none" class="navigator">
-          <image
-            mode="aspectFill"
-            class="image"
-            src="https://pcapi-xiaotuxian-front-devtest.itheima.net/miniapp/uploads/slider_2.jpg"
-          ></image>
-        </navigator>
-      </swiper-item>
-      <swiper-item>
-        <navigator url="/pages/index/index" hover-class="none" class="navigator">
-          <image
-            mode="aspectFill"
-            class="image"
-            src="https://pcapi-xiaotuxian-front-devtest.itheima.net/miniapp/uploads/slider_3.jpg"
-          ></image>
+          <image mode="aspectFill" class="image" :src="item.imgUrl"></image>
         </navigator>
       </swiper-item>
     </swiper>
     <!-- 指示点 -->
     <view class="indicator">
       <text
-        v-for="(item, index) in 3"
-        :key="item"
+        v-for="(item, index) in list"
+        :key="item.id"
         class="dot"
         :class="{ active: index === activeIndex }"
       ></text>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,13 +1,44 @@
 <template>
+  <!-- 自定义导航栏组件 -->
   <CustomNavbar />
-  <XtxSwiper />
+  <!-- 轮播图组件 -->
+  <XtxSwiper :list="bannerList" />
+  <!-- 轮播图组件 -->
+  <CategoryPanel :list="categoryList" />
   <view class="index"> </view>
 </template>
 
 <script setup lang="ts">
 import CustomNavbar from './components/CustomNavbar.vue'
+import CategoryPanel from './components/CategoryPanel.vue'
+import { getHomeBannerAPI, getHomeCategoryAPI } from '@/services/home'
+import type { BannerItem, CategoryItem } from '@/types/home'
+import { onLoad } from '@dcloudio/uni-app'
+import { ref } from 'vue'
+
+// 获取轮播图数据。。
+const bannerList = ref<BannerItem[]>([])
+const getHomeBannerData = async () => {
+  const res = await getHomeBannerAPI()
+  // console.log('----', res);
+  bannerList.value = res.result
+}
+// 首页分类数据。。
+const categoryList = ref<CategoryItem[]>([])
+const categoryListrData = async () => {
+  const res = await getHomeCategoryAPI()
+  // console.log('----', res);
+  categoryList.value = res.result
+}
+
+onLoad(() => {
+  getHomeBannerData()
+  categoryListrData()
+})
 </script>
 
 <style lang="scss">
-//
+page {
+  background-color: #f7f7f7;
+}
 </style>

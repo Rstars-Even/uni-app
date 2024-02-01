63cbf8dcf5bf95c5ef7dde92c8cf70958563618e
Rstars-Even <473753137@qq.com>
2024-02-01 18:18:47 +0800

    添加首页模块2。

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,18 +1,24 @@
 <template>
   <!-- 自定义导航栏组件 -->
   <CustomNavbar />
-  <!-- 轮播图组件 -->
-  <XtxSwiper :list="bannerList" />
-  <!-- 轮播图组件 -->
-  <CategoryPanel :list="categoryList" />
-  <view class="index"> </view>
+  <scroll-view scroll-y class="scroll-view">
+    <!-- 轮播图组件 -->
+    <XtxSwiper :list="bannerList" />
+    <!-- 首页分类组件 -->
+    <CategoryPanel :list="categoryList" />
+    <!-- 热门推荐组件 -->
+    <HotPanel :list="hotList" />
+    <!-- 猜你喜欢组件 -->
+    <XtxGuess />
+  </scroll-view>
 </template>
 
 <script setup lang="ts">
 import CustomNavbar from './components/CustomNavbar.vue'
 import CategoryPanel from './components/CategoryPanel.vue'
-import { getHomeBannerAPI, getHomeCategoryAPI } from '@/services/home'
-import type { BannerItem, CategoryItem } from '@/types/home'
+import HotPanel from './components/HotPanel.vue'
+import { getHomeBannerAPI, getHomeCategoryAPI, getHomeHotAPI } from '@/services/home'
+import type { BannerItem, CategoryItem, HotItem } from '@/types/home'
 import { onLoad } from '@dcloudio/uni-app'
 import { ref } from 'vue'
 
@@ -30,15 +36,29 @@ const categoryListrData = async () => {
   // console.log('----', res);
   categoryList.value = res.result
 }
+// 热门推荐数据。。
+const hotList = ref<HotItem[]>([])
+const hotListrData = async () => {
+  const res = await getHomeHotAPI()
+  // console.log('----', res);
+  hotList.value = res.result
+}
 
 onLoad(() => {
   getHomeBannerData()
   categoryListrData()
+  hotListrData()
 })
 </script>
 
 <style lang="scss">
 page {
   background-color: #f7f7f7;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+.scroll-view {
+  flex: 1;
 }
 </style>
